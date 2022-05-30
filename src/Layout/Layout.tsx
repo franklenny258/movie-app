007f3c06d3205f541a08e91313a48ba73fab93bb
franklenny258 <franklenny258@gmail.com>
2022-05-30 13:36:38 -0400

    feature(movie-app): initial files and initial layout implemented

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Wrapper, SideBarLayout, GridContentLayout } from "./styled"
+
+export const Layout = () => {
+    return (
+        <Wrapper>
+            <SideBarLayout>
+
+            </SideBarLayout>
+            <GridContentLayout>
+
+            </GridContentLayout>
+        </Wrapper>
+    )
+}
+
---
+++ src/Layout/styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    display: flex;
+    height: 100%;
+`;
+
+export const SideBarLayout = styled.nav`
+    border: 2px solid black;
+    flex: 1;
+    background-color: green;
+`;
+
+export const GridContentLayout = styled.aside`
+    border: 2px solid black;
+    flex: 5;
+    background-color: blue;
+`;
